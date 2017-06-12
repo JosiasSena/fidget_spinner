ea9a28455c91e12229bd035f76022cb351567d98
Josias Sena <josias.sena176@gmail.com>
2017-06-11 22:58:25 -0400

    first commit

---
+++ app/src/main/java/com/josiassena/fidgetspinner/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.josiassena.fidgetspinner
+
+import android.content.Context
+import android.os.Bundle
+import android.os.Vibrator
+import android.support.v4.view.animation.LinearOutSlowInInterpolator
+import android.support.v7.app.AppCompatActivity
+import android.view.KeyEvent
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var vibrator: Vibrator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        ivFidget.setOnTouchListener { _, event ->
+            when (event.action) {
+                KeyEvent.ACTION_DOWN -> {
+                    ivFidget.startAnimation(getSpinAnimation())
+                    startVibration()
+                }
+                KeyEvent.ACTION_UP -> {
+                    ivFidget.clearAnimation()
+                    stopVibration()
+                }
+            }
+            true
+        }
+    }
+
+    private fun getSpinAnimation(): RotateAnimation {
+        val spin = RotateAnimation(0f, 1000000f, Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f)
+
+        spin.duration = 4000
+        spin.repeatCount = Animation.INFINITE
+        spin.interpolator = LinearOutSlowInInterpolator()
+        spin.fillAfter = false
+
+        return spin
+    }
+
+    private fun startVibration() {
+        if (vibrator.hasVibrator()) {
+            vibrator.vibrate(500000)
+        }
+    }
+
+    private fun stopVibration() {
+        if (vibrator.hasVibrator()) {
+            vibrator.cancel()
+        }
+    }
+}
